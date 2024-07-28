f940200d4a7325db296a9e48a63d4388cecceb90
kingsharp21 <kingsharpn3@gmail.com>
2024-07-28 23:56:25 +0000

    first commi

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -1,4 +1,4 @@
-import React ,{ useState, useEffect, useRef} from "react";
+import React ,{ useState, useEffect} from "react";
 import Article from "../../components/Article";
 
 import feedService from "../../services/feedService";
